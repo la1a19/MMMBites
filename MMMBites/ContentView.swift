a384ebba8e4026040b083bcb55dc225540a9ca7e
zuntay <Yattin.Lee@student.uts.edu.au>
2026-06-03 13:26:44 +1000

    huhuhu

--- MMMBites/ContentView.swift
+++ MMMBites/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("bello, world!")
+            Text("bello, world!huhuhuhuhu")
         }
         .padding()
     }
