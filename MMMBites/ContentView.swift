7ebd6115828ba05008e07e427e663cf7b4dde1bb
Lila Lansang <lilalansang19@gmail.com>
2026-06-03 12:47:43 +1000

    Initial Commit

---
+++ MMMBites/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MMMBites
+//
+//  Created by Lila Lansang on 3/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MMMBites/MMMBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MMMBitesApp.swift
+//  MMMBites
+//
+//  Created by Lila Lansang on 3/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct MMMBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
